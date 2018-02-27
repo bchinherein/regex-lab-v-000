31b69915bbb049f6d8bcc6d31934d855cae559e1
Brandon Chin <brandonchin@me.com>
2018-02-27 21:01:30 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -17,5 +17,5 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  phone.match(/(\d{10})|(\b|\D\d{3}\b|\D\d{3}\b|\D\d{4})/) ? true : false
+  phone.match(/((\b*|\D*)(\d{3})(\b*|\D*)(\d{3})(\b*|\D)(\d{4}))/) ? true : false
 end
