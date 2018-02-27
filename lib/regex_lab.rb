8be5062f9760c785598b94b1f0df9dbabf24c1a5
Brandon Chin <brandonchin@me.com>
2018-02-27 19:08:01 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,13 +1,15 @@
+require 'pry'
 def starts_with_a_vowel?(word)
-
+  word.match(/\b[aeiouAEIOU]/) ? true : false
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
-end
+  text.scan(\b+un+(...)+ing\b)
+  binding.pry
+end;
 
 def words_five_letters_long(text)
-
+  
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
